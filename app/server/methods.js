f4be8044acd2c611b33ea469a26aae7c438e8354
Banti Gheneti <banti@gheneti.com>
2016-01-21 21:54:07 -0500

    backend methods profile update change

--- app/server/methods.js
+++ app/server/methods.js
@@ -1,22 +1,27 @@
 Meteor.methods({
-  addProfile: function (profile) {
-    // Make sure the user is logged in before inserting a task
-  /*  if (! Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    } */
-    profile.owner=Meteor.userId();
-    Profiles.insert(profile);
-  },
   deleteProfile: function () {
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
     Profiles.remove({owner: Meteor.userId});
   },
-  editProfile: function (profile) {
+  updateProfile: function (profile) {
     if (! Meteor.userId() || (Meteor.userId()!==profile.owner)) {
       throw new Meteor.Error("not-authorized");
     }
-    Profiles.update({owner: Meteor.userId},{$addToSet: {name:profile.name,country:profile.country,description:profile.description,posts:profile.posts}});
+    if (Profiles.findOne({owner: Meteor.userId()})){
+      Profiles.update(
+        {owner: Meteor.userId},
+        {$addToSet:{
+          name:profile.name,
+          country:profile.country,
+          description:profile.description,
+          posts:profile.posts
+        }}
+      );
+    } else {
+      profile.owner=Meteor.userId();
+      Profiles.insert(profile);
+    }
   }
 });
