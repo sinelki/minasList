a505f0ae700910f5155f30dad96127d475ac73f9
Christine Konicki <ckonicki@mit.edu>
2016-01-25 20:01:26 -0500

    added social bar to all pages

--- app/client/profiles.js
+++ app/client/profiles.js
@@ -1,1 +1,7 @@
 Meteor.subscribe('Profiles');
+var filter_div = document.getElementById('filter-by');
+filter_div.style.cursor = 'pointer';
+filter_div.onclick = function() {
+    // do something...
+    var filter_choice = prompt("Please select your choice of filter");
+};
