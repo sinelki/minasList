03812ce4e8720b80b0f8a99b0661b925e9e3c996
Christine Konicki <ckonicki@mit.edu>
2016-01-27 00:20:49 -0500

    added filter-collections

--- app/client/profiles.js
+++ app/client/profiles.js
@@ -30,9 +30,11 @@ Template.profileHeaders.helpers({
 		console.log(drop);
 		var key = drop.options[drop.selectedIndex].value;
 		var textbox = document.getElementById("choice");
-		var value = textbox.value;
-		var selector = {};
-		selector[key] = value;
-		return Profiles.find(selector).fetch();
+		var val = textbox.value;
+		if (key == "country") {
+			Router.go('/profiles');
+			this.render('Profiles');
+		}
+		console.log("You have selected " + key);
 	}
 });
