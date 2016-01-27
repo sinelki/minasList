9d3494368dae2fd041a0248a89851c72510a5e8f
Christine Konicki <ckonicki@mit.edu>
2016-01-26 19:38:21 -0500

    removed extra css

---
+++ app/client/app.js
@@ -0,0 +1,35 @@
+if (Meteor.isClient) {
+  Template.dashboard.events({
+    'click .logout': function(event) {
+      event.preventDefault();
+      Meteor.logout();
+    }
+  });
+
+  Template.register.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var emailVar = event.target.registerEmail.value;
+      var passwordVar = event.target.registerPassword.value;
+      Accounts.createUser({
+        email: emailVar, 
+        password: passwordVar
+      });
+    }
+  });
+
+  Template.login.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var emailVar = event.target.loginEmail.value;
+      var passwordVar = event.target.loginPassword.value;
+      Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
