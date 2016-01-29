4239c981d1c9a76177081ece5ed3b5fc90b46833
amadexodus <christinejkon@gmail.com>
2016-01-29 01:12:35 -0500

    added comments

--- app/client/login.js
+++ app/client/login.js
@@ -1,4 +1,5 @@
 Template.login.events({
+    // function to determine routing to the next page depending on login outcome
     'submit form': function(event){
         event.preventDefault();
         console.log(event);
@@ -9,13 +10,16 @@ Template.login.events({
            console.log(Meteor.user());
             // login is now complete so do what you'd like.
             console.log(Meteor.user());
+            // throw an error if the user who attempted to log in does not have an account
             if (! Meteor.userId()) {
               throw new Meteor.Error("not-authorized");
             }
+            // go to the admin page if the user logging in is the administrator
             else if (Meteor.user().username==='admin'){
               console.log('yay');
               Router.go('/admin');
             }
+            // go to the profile page of the person who successfully logged in
             else{
               Router.go('/editProfile/'+Meteor.userId());
             }
